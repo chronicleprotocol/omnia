07199a0ec6170da558658ca422b5ad0dfc9d03a8
Paweł Zaremba <pawzar@gmail.com>
2022-04-28 18:56:55 +0200

    Move ethsign to omnia

---
+++ ethsign/default.nix
@@ -0,0 +1,16 @@
+{ lib, buildGoModule }:
+
+buildGoModule rec {
+  name = "ethsign-${version}";
+  version = "0.17.1";
+
+  src = ./.;
+
+  vendorSha256 = "1zbsq1lyqinyzv5x4p1xgkxsyb7y92fbmf44gyaix34xrag5s27m";
+
+  meta = {
+    homepage = http://github.com/dapphub/dapptools;
+    description = "Make raw signed Ethereum transactions";
+    license = [lib.licenses.agpl3];
+  };
+}
