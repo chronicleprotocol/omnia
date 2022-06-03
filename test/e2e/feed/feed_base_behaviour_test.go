2a322298457967c0e9fc4eb3f23eae3400f0dbc6
Paweł Zaremba <pawzar@gmail.com>
2022-06-03 16:35:00 +0200

    add assertion messages

--- test/e2e/feed/feed_base_behaviour_test.go
+++ test/e2e/feed/feed_base_behaviour_test.go
@@ -139,29 +139,29 @@ func (s *FeedBaseBehaviourE2ESuite) TestBaseSuccessBehaviour() {
 		Add(origin.NewExchange("kraken").WithSymbol("XXBT/ZUSD").WithPrice(1)).
 		Deploy(s.API)
 
-	s.Require().NoError(err)
+	s.Require().NoError(err, "a")
 
 	err = s.Omnia.Start()
-	s.Require().NoError(err)
+	s.Require().NoError(err, "b")
 
 	ch, err := s.Transport.ReadChan()
-	s.Require().NoError(err)
-	s.Require().NotNil(ch)
+	s.Require().NoError(err, "c")
+	s.Require().NotNil(ch, "d")
 
 	msg, err := s.Transport.WaitMsg(15 * time.Second)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "e")
 
 	var price e2e.PriceMessage
 
 	err = json.Unmarshal([]byte(msg), &price)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "f")
 
-	s.Assert().Equal("BTCUSD", price.Price.Wat)
-	s.Assert().Equal("1000000000000000000", price.Price.Val)
-	s.Assert().Greater(time.Now().Unix(), price.Price.Age)
-	s.Assert().NotEmpty(price.Price.R)
-	s.Assert().NotEmpty(price.Price.S)
-	s.Assert().NotEmpty(price.Price.V)
+	s.Assert().Equal("BTCUSD", price.Price.Wat, "g")
+	s.Assert().Equal("1000000000000000000", price.Price.Val, "h")
+	s.Assert().Greater(time.Now().Unix(), price.Price.Age, "i")
+	s.Assert().NotEmpty(price.Price.R, "j")
+	s.Assert().NotEmpty(price.Price.S, "k")
+	s.Assert().NotEmpty(price.Price.V, "l")
 
 	// Next call
 	// Setup price for BTC/USD
@@ -174,20 +174,20 @@ func (s *FeedBaseBehaviourE2ESuite) TestBaseSuccessBehaviour() {
 		Add(origin.NewExchange("kraken").WithSymbol("XXBT/ZUSD").WithPrice(2)).
 		Deploy(s.API)
 
-	s.Require().NoError(err)
+	s.Require().NoError(err, "a2")
 
 	msg, err = s.Transport.WaitMsg(15 * time.Second)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "b2")
 
 	err = json.Unmarshal([]byte(msg), &price)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "c2")
 
-	s.Assert().Equal("BTCUSD", price.Price.Wat)
-	s.Assert().Equal("2000000000000000000", price.Price.Val)
-	s.Assert().Greater(time.Now().Unix(), price.Price.Age)
-	s.Assert().NotEmpty(price.Price.R)
-	s.Assert().NotEmpty(price.Price.S)
-	s.Assert().NotEmpty(price.Price.V)
+	s.Assert().Equal("BTCUSD", price.Price.Wat, "d2")
+	s.Assert().Equal("2000000000000000000", price.Price.Val, "e2")
+	s.Assert().Greater(time.Now().Unix(), price.Price.Age, "f2")
+	s.Assert().NotEmpty(price.Price.R, "g2")
+	s.Assert().NotEmpty(price.Price.S, "h2")
+	s.Assert().NotEmpty(price.Price.V, "i2")
 
 	// 3rd step
 	// Setup price for BTC/USD
@@ -200,18 +200,18 @@ func (s *FeedBaseBehaviourE2ESuite) TestBaseSuccessBehaviour() {
 		Add(origin.NewExchange("kraken").WithSymbol("XXBT/ZUSD").WithPrice(3)).
 		Deploy(s.API)
 
-	s.Require().NoError(err)
+	s.Require().NoError(err, "a3")
 
 	msg, err = s.Transport.WaitMsg(15 * time.Second)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "b3")
 
 	err = json.Unmarshal([]byte(msg), &price)
-	s.Require().NoError(err)
+	s.Require().NoError(err, "c3")
 
-	s.Assert().Equal("BTCUSD", price.Price.Wat)
-	s.Assert().Equal("3000000000000000000", price.Price.Val)
-	s.Assert().Greater(time.Now().Unix(), price.Price.Age)
-	s.Assert().NotEmpty(price.Price.R)
-	s.Assert().NotEmpty(price.Price.S)
-	s.Assert().NotEmpty(price.Price.V)
+	s.Assert().Equal("BTCUSD", price.Price.Wat, "d3")
+	s.Assert().Equal("3000000000000000000", price.Price.Val, "e3")
+	s.Assert().Greater(time.Now().Unix(), price.Price.Age, "f3")
+	s.Assert().NotEmpty(price.Price.R, "g3")
+	s.Assert().NotEmpty(price.Price.S, "h3")
+	s.Assert().NotEmpty(price.Price.V, "i3")
 }
